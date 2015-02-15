7faee6e9f3dc588abed58572f6ff5d58c7ac87cc
Kyle McKee <mckee.kyle@gmail.com>
2015-02-15 09:26:31 -0500

    Refactored to use attr_accessor.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Calculator do
+  subject { Calculator.new }
   it 'knows the bedtime' do
-    subject = Calculator.new
     time = Time.now
     subject.bedtime = time
     expect(subject.bedtime).to eql time
