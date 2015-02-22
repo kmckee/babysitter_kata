10661c83eee38ed07cfb1f956142ded3816a91be
Kyle McKee <mckee.kyle@gmail.com>
2015-02-22 11:37:28 -0500

    Extracted helper methods to help with spec readability.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe Calculator do
   let(:arbitrary_time) { '5:00pm'.to_t }
   let(:hours_mock) { double('hours_mock') }
+  def set_hours_worked hours
+    allow(hours_mock).to receive(:hours_between).and_return(hours)
+  end
+  def set_rate rate
+    subject.rate_before_bedtime = BigDecimal.new(rate.to_s)
+  end
   
   subject { Calculator.new(hours_mock) }
   
@@ -17,21 +23,22 @@ describe Calculator do
     expect(subject.rate_before_bedtime).to eql rate
   end
   it 'can calculate a total for 1 hour before bedtime' do
-    allow(hours_mock).to receive(:hours_between).and_return(1)
-    subject.rate_before_bedtime = BigDecimal.new('12.00')
+    set_rate(12)
+    set_hours_worked(1)
     total = subject.get_cost_for(arbitrary_time, arbitrary_time)
     expect(total).to eql (12 * 1)
   end
   it 'can calculate a total for 2 hours before bedtime' do
-    allow(hours_mock).to receive(:hours_between).and_return(2)
-    subject.rate_before_bedtime = BigDecimal.new('12.00')
+    set_rate(12)
+    set_hours_worked(2)
     total = subject.get_cost_for(arbitrary_time, arbitrary_time)
     expect(total).to eql (12 * 2)
   end
   it 'respects different rates' do
-    allow(hours_mock).to receive(:hours_between).and_return(1)
-    subject.rate_before_bedtime = BigDecimal.new('18.00')
+    set_rate(18)
+    set_hours_worked(1)
     total = subject.get_cost_for(arbitrary_time, arbitrary_time)
     expect(total).to eql (18 * 1)
   end
 end
+
