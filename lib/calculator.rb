02bf20a5460814db0a0c4b4c330b213cb0aebe9f
Kyle McKee <mckee.kyle@gmail.com>
2015-02-22 09:19:17 -0500

    Extracted class for HoursWorked calculations.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,11 +1,19 @@
 class Calculator
   attr_accessor :bedtime, :rate_before_bedtime
+  def initialize
+    @hours_calculator = HoursWorkedCalculator.new
+  end
 
-  NUMBER_OF_SECONDS_IN_AN_HOUR = 3600
-  
   def get_cost_for(start_time, end_time)
+    elapsed_hours = @hours_calculator.hours_between(start_time, end_time)
+    total = rate_before_bedtime * elapsed_hours
+  end
+end
+
+class HoursWorkedCalculator
+  NUMBER_OF_SECONDS_IN_AN_HOUR = 3600
+  def hours_between(start_time, end_time)
     elapsed_time_in_seconds = end_time - start_time
     elapsed_hours = elapsed_time_in_seconds / NUMBER_OF_SECONDS_IN_AN_HOUR
-    total = rate_before_bedtime * elapsed_hours
   end
 end
