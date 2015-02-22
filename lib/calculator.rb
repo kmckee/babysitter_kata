cbace41c793c3f822dd0908da95c593e74f1b27e
Kyle McKee <mckee.kyle@gmail.com>
2015-02-22 09:37:17 -0500

    Splt class into separate file.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -9,11 +9,3 @@ class Calculator
     total = rate_before_bedtime * elapsed_hours
   end
 end
-
-class HoursWorkedCalculator
-  NUMBER_OF_SECONDS_IN_AN_HOUR = 3600
-  def hours_between(start_time, end_time)
-    elapsed_time_in_seconds = end_time - start_time
-    elapsed_hours = elapsed_time_in_seconds / NUMBER_OF_SECONDS_IN_AN_HOUR
-  end
-end
---
+++ lib/hours_worked_calculator.rb
@@ -0,0 +1,7 @@
+class HoursWorkedCalculator
+  NUMBER_OF_SECONDS_IN_AN_HOUR = 3600
+  def hours_between(start_time, end_time)
+    elapsed_time_in_seconds = end_time - start_time
+    elapsed_hours = elapsed_time_in_seconds / NUMBER_OF_SECONDS_IN_AN_HOUR
+  end
+end
